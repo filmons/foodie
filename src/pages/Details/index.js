c06862c7c290788daf324405c3545f7b70007806
Mayahi Adel <adel.mayahii@gmail.com>
2021-04-14 16:39:24 +0200

    create route pages

---
+++ src/pages/Details/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+class Details extends React.Component {
+    render(){
+        return (
+            <h1> Details page</h1>
+        )
+    }
+}
+
+export default Details
